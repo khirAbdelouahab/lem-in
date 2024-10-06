79d030ca86176005d70cf4d636d41a34dc5d807c
Zakaria Abdelali <104455697+heyZakaria@users.noreply.github.com>
2024-10-06 21:35:16 +0100

    zak readdata.go

--- ReadData/readdata.go
+++ ReadData/readdata.go
@@ -1,1 +1,78 @@
-package readdata
+package readata
+
+import (
+	"regexp"
+	"strings"
+)
+
+type Start struct {
+	start      string
+	startCheck bool
+	x          string
+	y          string
+}
+
+type End struct {
+	end      string
+	endCheck bool
+	x        string
+	y        string
+}
+
+func ExtractStartAndEnd(data []string) {
+	var s Start
+	var e End
+	for i, v := range data {
+		if v == "##start" && i != len(data)-1 && !s.startCheck {
+			hold := strings.Split(data[i+1], " ")
+			s.start = hold[0]
+			s.startCheck = true
+		}
+		if v == "##end" && i != len(data)-1 && !e.endCheck {
+			hold := strings.Split(data[i+1], " ")
+			e.end = hold[0]
+			e.endCheck = true
+		}
+	}
+}
+
+func ExtractEdgeList(data []string) [][]string {
+	var EdgeList [][]string
+	for _, v := range data {
+		pattern := regexp.MustCompile(`^[a-zA-Z0-9]+-[a-zA-Z0-9$]+`)
+		edge := pattern.FindAllString(v, -1)
+		if len(edge) > 0 {
+			EdgeList = append(EdgeList, edge)
+		}
+	}
+
+	return EdgeList
+}
+
+func EdgeListToAdjList(List [][]string) map[string][]string {
+	var hold [][]string
+	m := make(map[string][]string)
+	for _, Node := range List {
+		for _, v := range Node {
+
+			pattern := regexp.MustCompile(`^[a-zA-Z0-9]+-[a-zA-Z0-9]+$`)
+			found := pattern.FindAllString(v, -1)
+			if len(found) > 0 {
+				h := strings.Split(found[0], "-")
+				hold = append(hold, h)
+			}
+
+		}
+	}
+
+	for _, v := range hold {
+
+		m[v[0]] = append(m[v[0]], v[1])
+
+		m[v[1]] = append(m[v[1]], v[0])
+
+	}
+	//fmt.Println(m)
+
+	return m
+}
